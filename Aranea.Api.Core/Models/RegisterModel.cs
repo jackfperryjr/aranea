dfa2622f4703bb37cf8dc4ea83601caaea44454b
jackfperryjr <jack.franklin.perryjr@gmail.com>
2020-07-02 23:15:31 -0400

    Changed error message again

--- Aranea.Api.Core/Models/RegisterModel.cs
+++ Aranea.Api.Core/Models/RegisterModel.cs
@@ -18,7 +18,7 @@ namespace Aranea.Api.Core.Models
         public DateTime JoinDate { get; set; }
 
         [Required]
-        [StringLength(12, ErrorMessage = "the {0} must be between {2} and {1} characters", MinimumLength = 6)]
+        [StringLength(12, ErrorMessage = "your password must be between {2} and {1} characters", MinimumLength = 6)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
